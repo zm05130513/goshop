1f03a1a7ce89c693bd1b23bcbcbfb62f27a1199d
zm05130513 <zm05130513@163.com>
2020-12-25 12:01:05 +0800

    BaseFragment.java

--- app/src/main/java/com/example/goshopkuang/base/BaseFragment.java
+++ app/src/main/java/com/example/goshopkuang/base/BaseFragment.java
@@ -1,4 +1,73 @@
 package com.example.goshopkuang.base;
 
-public class BaseFragment {
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.goshopkuang.interfaces.IBaseView;
+import com.example.goshopkuang.interfaces.IPresenter;
+
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
+public abstract class BaseFragment<V extends IBaseView, P extends IPresenter> extends Fragment implements IBaseView {
+
+    protected P presenter;
+    protected Context context;
+    Unbinder unbinder;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(getLayout(), null);
+        context = this.getContext();
+        unbinder = ButterKnife.bind(this, view);
+        initView(view);
+        presenter = createPresenter();
+        if (presenter != null) presenter.attchView(this);
+        initData();
+        return view;
+    }
+
+    //获取布局
+    protected abstract int getLayout();
+
+    //初始化布局
+    protected abstract void initView(View view);
+
+    //初始化数据
+    protected abstract void initData();
+
+    //创建P
+    protected abstract P createPresenter();
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (presenter!=null){
+            presenter.attchView(this);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if (presenter != null) {
+            presenter.detachView();
+        }
+        if (unbinder != null) {
+            unbinder.unbind();
+        }
+    }
+
+   /* @Override
+    public void showErrMsg(String err) {
+
+    }*/
 }
