208f35cce6f9f8cc08c8c1fc643bbfa7ed5a03d1
zm05130513 <zm05130513@163.com>
2020-12-27 11:54:55 +0800

    TopicFragmeng.java

--- app/src/main/java/com/example/goshopkuang/view/topic/TopicFragment.java
+++ app/src/main/java/com/example/goshopkuang/view/topic/TopicFragment.java
@@ -2,6 +2,7 @@ package com.example.goshopkuang.view.topic;
 
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -13,11 +14,13 @@ import com.example.goshopkuang.base.BaseFragment;
 import com.example.goshopkuang.interfaces.IPresenter;
 import com.example.goshopkuang.interfaces.topic.TopicContract;
 import com.example.goshopkuang.model.bean.topic.TopicBean;
+import com.example.goshopkuang.presenter.topic.TopicPresenter;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 import butterknife.Unbinder;
 
 public class TopicFragment extends BaseFragment implements TopicContract.View {
@@ -54,21 +57,45 @@ public class TopicFragment extends BaseFragment implements TopicContract.View {
 
     @Override
     protected void initData() {
-
+        ((TopicPresenter) presenter).topic();
     }
 
     @Override
     protected IPresenter createPresenter() {
-        return null;
+        return new TopicPresenter();
     }
 
     @Override
     public void showErrMsg(String err) {
-
+        Toast.makeText(getActivity(), err, Toast.LENGTH_SHORT).show();
     }
+
+    private int page = 2;
 
     @Override
     public void topicDataReturn(TopicBean msg) {
+        list.clear();
+        list.addAll(msg.getData().getData());
+        topicAdapter.notifyDataSetChanged();
+    }
 
+    @OnClick({R.id.btn_previous, R.id.btn_next})
+    public void onViewClicked(View view) {
+        switch (view.getId()) {
+            case R.id.btn_previous:
+
+                ((TopicPresenter) presenter).topic();
+                btnPrevious.setSelected(true);
+                btnNext.setSelected(false);
+
+                break;
+            case R.id.btn_next:
+
+                ((TopicPresenter) presenter).topic(page);
+                btnPrevious.setSelected(false);
+                btnNext.setSelected(true);
+
+                break;
+        }
     }
 }
